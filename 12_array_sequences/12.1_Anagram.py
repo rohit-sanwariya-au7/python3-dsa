422fbcbb279b4a696817cc4a3536a7cac4d06aed
rohitsanwariyaIT <rohitsanwariya1995@gmail.com>
2020-02-03 02:42:26 +0530

    empty folders added

---
+++ 12_array_sequences/12.1_Anagram.py
@@ -0,0 +1,10 @@
+def anagram(string1,string2):
+    stringNew1=string1.replace(' ','').lower()
+    stringNew2=string2.replace(' ','').lower()
+    if (len(stringNew1)!=len(stringNew2)):
+        return False
+    count={}
+    for letter in stringNew1:
+        if(count[letter]==)
+        count[letter]+=1
+    
